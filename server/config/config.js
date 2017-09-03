ece8a1845f3ff3080a04e4f7816bc14a219e62ac
nmarcialc <nataniel_natas@hotmail.com>
2017-09-03 11:55:48 -0700

    setup skeleton and folder srtucture

---
+++ server/config/config.js
@@ -0,0 +1,12 @@
+var path = require('path');
+var rootPath = path.normalize(__dirname +'/../../');
+module.exports ={
+    development:{
+        rootPath :rootPath,
+        port: process.env.PORT || 5000
+    },
+    production:{
+        rootPath :rootPath,
+        port: process.env.PORT || 80
+    }
+}
